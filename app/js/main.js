51e6fdfcb2ee7015c7ccc0ed8d6da36b3493279c
williamjzhao <wzhao2017@gmail.com>
2018-09-29 22:52:21 -0500

    stuff

--- app/js/main.js
+++ app/js/main.js
@@ -131,6 +131,7 @@ function onDOMContentLoad() {
 
     document.getElementById('submitCheck').addEventListener('click',() => {
       token = document.getElementById('checker').value;
+      console.log(token);
       if(document.getElementById('checker').value === process.env.API_SECRET) {
         scan();
         console.log('scan');
